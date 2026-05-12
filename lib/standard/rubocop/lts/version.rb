5b984939662fbd24e864949c54a1cc704d07c38c
autobolt <autobots@9thbit.net>
2026-05-12 13:30:34 -0600

    Release v2.0.1

--- lib/standard/rubocop/lts/version.rb
+++ lib/standard/rubocop/lts/version.rb
@@ -7,7 +7,7 @@ module Standard
       module Version
         # Bump VERSION manually for each new release!
         # See: CONTRIBUTING.md
-        VERSION = "2.0.0"
+        VERSION = "2.0.1"
       end
     end
   end
