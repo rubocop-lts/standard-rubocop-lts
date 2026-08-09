a69e7a1b23be699a985c5e185332feb63bb04335
Peter H. Boling <peter.boling@gmail.com>
2026-08-09 09:45:56 -0600

    🔧 Require Ruby API compatibility plugin release

--- lib/standard/rubocop/lts/version.rb
+++ lib/standard/rubocop/lts/version.rb
@@ -6,7 +6,7 @@ module Standard
       # Version namespace for this gem.
       module Version
         # Current gem version.
-        VERSION = "2.0.10"
+        VERSION = "2.0.11"
       end
       # Current gem version exposed at the traditional constant location.
       VERSION = Version::VERSION # Traditional Constant Location
