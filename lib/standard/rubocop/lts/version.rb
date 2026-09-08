ff2390b466a067c346e583c34e662a1209fb4513
Peter H. Boling <peter.boling@gmail.com>
2026-09-07 23:00:17 -0400

    🔖 Bump gem version

--- lib/standard/rubocop/lts/version.rb
+++ lib/standard/rubocop/lts/version.rb
@@ -6,7 +6,7 @@ module Standard
       # Version namespace for this gem.
       module Version
         # Current gem version.
-        VERSION = "2.0.11"
+        VERSION = "2.0.12"
       end
       # Current gem version exposed at the traditional constant location.
       VERSION = Version::VERSION # Traditional Constant Location
